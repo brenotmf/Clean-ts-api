7409f2b572000b2819b780152da5adf5c530f495
Breno França <breno.franca@roit.com.br>
2021-01-04 11:02:27 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,14 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+
 export class SignUpcontroller {
-  handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+  handle (htttpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!htttpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,5 @@
-import { MissingParamError } from '../erros/missing-param-error'
-
 import { HttpResponse } from '../protocols/http'
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
