e005e9c4c3617d009265d6e301bd139c6d1ad34d
Breno França <breno.franca@roit.com.br>
2021-01-27 11:40:53 -0300

    feat: ensure mongodb reconnect if client  is down

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup'
 import { adaptRoute } from './adapters/express-route-adapter'
 
-export default (router: Router): void => {
+export default (router: Router):void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
